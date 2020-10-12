837f9be9bd1135d70c98001adac4d6c1ee069e56
dimple0410 <72303980+dimple0410@users.noreply.github.com>
2020-10-12 18:28:20 +0530

    Update Hello.java

--- JenkinsDemoProgram/src/practise/demo/Hello.java
+++ JenkinsDemoProgram/src/practise/demo/Hello.java
@@ -6,6 +6,10 @@ public class Hello {
 	{
 		System.out.println("This is Constructor...");
 	}
+	public void method1()
+	{
+		System.out.println("This is Method 1...");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("In Main Hello World!!!");
