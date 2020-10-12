97913b48d1bb3dff1e8afc19939a4dfd7714cc96
dimpl <dimpl@LAPTOP-J2EAHDLS>
2020-10-12 18:26:55 +0530

    Added Constructor

--- JenkinsDemoProgram/src/practise/demo/Hello.java
+++ JenkinsDemoProgram/src/practise/demo/Hello.java
@@ -2,6 +2,10 @@ package practise.demo;
 
 public class Hello {
 
+	public Hello()
+	{
+		System.out.println("This is Constructor...");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("In Main Hello World!!!");
